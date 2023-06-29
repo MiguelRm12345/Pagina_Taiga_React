04ce31b446dee13efeb1163cb2a5fabae2730d68
MiguelRm12345 <miguelrm2407yiu@gmail.com>
2023-06-29 16:42:46 -0500

    tercercommit

--- taiga-react/src/App.jsx
+++ taiga-react/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
       <Slider />
 
-      <Proyecto infoproyecto = "Taiga es un proyecto enfocado a la conservacion de zonas  verdes o boscosas, nos centramos en el bosque de Monte Carlo ubicado en Manrique Jardin para ser enfasis en el poco cuidado que recibe este y fomentar el cuidado de los bosques.Queremos incentivar a futuras generaciones para que cuiden el medio ambiente y los arboles que son nuetra principal fuente de oxigeno en el ambiente, esto se tiene pensado en mensajes, campañas y directos habando sobre los impactos ambientales que generan desastres tales como deforestacion, incendios, catastrofes naturales, etc."/>
+      <Proyecto infoproyecto = "Taiga es un proyecto enfocado a la conservación de zonas  verdes o boscosas, nos centramos en el bosque de Monte Carlo ubicado en Manrique Jardin para ser enfasis en el poco cuidado que recibe este y fomentar el cuidado de los bosques.Queremos incentivar a futuras generaciones para que cuiden el medio ambiente y los arboles que son nuetra principal fuente de oxigeno en el ambiente, esto se tiene pensado en mensajes, campañas y directos habando sobre los impactos ambientales que generan desastres tales como deforestacion, incendios, catastrofes naturales, etc."/>
 
       <Videojuego informacion = "Lider de grupo y programador, su fuerte es la creacion de contenido en el ambito de programacion de paginas web, diseño de landing pages y uso de variables." />
 
